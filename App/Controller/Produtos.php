badcd73f5a5b29586e927308ec0797ee1ff64f0b
Eduardo Caciano <eduardocaciano15@hotmail.com>
2021-06-23 07:25:50 -0300

    18/06/2021

---
+++ App/Controller/Produtos.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Core\Controller;
+
+class Produtos extends Controller{
+    public function index(){
+
+        $produtoModel= $this->model("Produto");
+
+        $dados = $produtoModel->listarTodos();
+
+        $this->view("produtos/index", $dados);
+
+    }
+
+    // bucar pelo id
+
+    // retornar a view de edição
+
+    // salvar a edição
+
+    // retornar a view de inserção
+
+    // salvar a inserção
+
+    // deletar
+}
